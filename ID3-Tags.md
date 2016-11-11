65744ef10917d05f9f35f4197c55408b7d135314
Electron <devversitymail@gmail.com>
2016-11-12 04:38:15 +0530

    ID3 Tags

---
+++ ID3-Tags.md
@@ -0,0 +1,16 @@
+#### ID3 Tags
+
+## Sure
+- TALB = Album Name (Smoke + Mirrors)
+- TPE1 = Artist Name (Imagine Dragons)
+- TPE2 = Artist Name (Imagine Dragons)
+- TSOP = Artist Name (Imagine Dragons)
+- USLT = Lyrics of Track
+- TDRC = Date or Year of Release (2015-05-20 or 2015)
+- TPUB = Publishing Label/Company (Interscope Records/KIDinaKORNER)
+- TIT2 = Title of Track (Hopeless Opus)
+
+
+## Not Sure
+- TCON = Genre of Track
+- TXXX : <Custom Name> = Custom Tags 
--- tags.py
+++ tags.py
@@ -1,14 +1,49 @@
-'''
-ID3 tags Fun
-'''
 from mutagen.id3 import ID3
 
-audio = ID3("Hopeless Opus.mp3")
-tags = audio.items()
-# print(type(tags))
-for tag in tags:
-    if tag[0] == 'APIC:':
-        print("cover art image")
-        # this is becuse we don't want to parse Cover Art image as Text'
-    else:
-        print(str(tag).encode(encoding='utf_8'))
+
+def metadata():
+    metaText = []
+
+    # audio = ID3("xxx.mp3")
+    audio = ID3("Hopeless Opus.mp3")
+    # audio = ID3("Broken Arrows.mp3")
+    tags = audio.items()
+
+    # for tag in tags:
+    #     if tag[0] == 'APIC:':
+    #         pass
+    #         # print("cover art image")
+    #         # this is becuse we don't want to parse Cover Art image as Text'
+    #     else:
+    #         print("\n")
+    #         print(str(tag[0]).encode(encoding='utf_8'))
+    #         # print(str(tag[1]).encode(encoding='utf_8'))
+
+    for tag in tags:
+        if (tag[0] == 'USLT::eng'):
+            metaText.append(str(tag[1]).encode(encoding='utf_8'))
+        if (tag[0] == 'TALB'):
+            metaText.append(str(tag[1]).encode(encoding='utf_8'))
+        if (tag[0] == 'TPE1'):
+            metaText.append(str(tag[1]).encode(encoding='utf_8'))
+        if (tag[0] == 'TPE2'):
+            metaText.append(str(tag[1]).encode(encoding='utf_8'))
+        if (tag[0] == 'TSOP'):
+            metaText.append(str(tag[1]).encode(encoding='utf_8'))
+        if (tag[0] == 'TIT2'):
+            metaText.append(str(tag[1]).encode(encoding='utf_8'))
+        if (tag[0] == 'TCON'):
+            metaText.append(str(tag[1]).encode(encoding='utf_8'))
+    return metaText
+
+if __name__ == '__main__':
+    meta = metadata()
+    final = []
+    # print(meta)
+    for data in meta:
+        split = data.split()
+        for text in split:
+            final.append(text)
+
+    for text in final:
+        print(text)
