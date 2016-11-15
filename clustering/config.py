fbd1910b8513a582906890371fd15cd4b95d0cb0
Electron <devversitymail@gmail.com>
2016-11-15 07:15:24 +0530

    removed news clustering part

--- clustering/config.py
+++
@@ -1,7 +0,0 @@
-guardURL = 'http://content.guardianapis.com/search?'
-nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'
-key_g = '' # #insert your The Guardian api-key
-key_nyt = '' # #insert your NYT api-key
-
-api_urls = [guardURL, nytURL]
-api_keys = [key_g, key_nyt]
