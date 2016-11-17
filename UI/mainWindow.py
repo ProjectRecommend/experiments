5f9dba82f5141d9c5663ccc3ea245ab8ed9919ab
Electron <devversitymail@gmail.com>
2016-11-17 20:01:04 +0530

    Updated main WIndow UI

--- UI/mainWindow.py
+++ UI/mainWindow.py
@@ -56,9 +56,9 @@ class Ui_MainWindow(object):
         self.gridLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
         self.gridLayoutWidget_3.setGeometry(QtCore.QRect(0, 450, 701, 131))
         self.gridLayoutWidget_3.setObjectName("gridLayoutWidget_3")
-        self.gridLayout_3 = QtWidgets.QGridLayout(self.gridLayoutWidget_3)
-        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.Player = QtWidgets.QGridLayout(self.gridLayoutWidget_3)
+        self.Player.setContentsMargins(0, 0, 0, 0)
+        self.Player.setObjectName("Player")
         self.Timeline = QtWidgets.QHBoxLayout()
         self.Timeline.setObjectName("Timeline")
         self.CurrentTime = QtWidgets.QLabel(self.gridLayoutWidget_3)
@@ -141,13 +141,13 @@ class Ui_MainWindow(object):
         self.TotalTime.setIndent(2)
         self.TotalTime.setObjectName("TotalTime")
         self.Timeline.addWidget(self.TotalTime)
-        self.gridLayout_3.addLayout(self.Timeline, 1, 0, 1, 1)
-        self.gridLayout_2 = QtWidgets.QGridLayout()
-        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.Player.addLayout(self.Timeline, 1, 0, 1, 1)
+        self.SongInfoAndControls = QtWidgets.QGridLayout()
+        self.SongInfoAndControls.setObjectName("SongInfoAndControls")
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout_2.addItem(spacerItem, 0, 1, 1, 1)
+        self.SongInfoAndControls.addItem(spacerItem, 0, 1, 1, 1)
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout_2.addItem(spacerItem1, 1, 1, 1, 1)
+        self.SongInfoAndControls.addItem(spacerItem1, 1, 1, 1, 1)
         self.SongTitle = QtWidgets.QLabel(self.gridLayoutWidget_3)
         font = QtGui.QFont()
         font.setFamily("Open Sans")
@@ -157,7 +157,7 @@ class Ui_MainWindow(object):
         self.SongTitle.setFont(font)
         self.SongTitle.setIndent(5)
         self.SongTitle.setObjectName("SongTitle")
-        self.gridLayout_2.addWidget(self.SongTitle, 0, 0, 1, 1)
+        self.SongInfoAndControls.addWidget(self.SongTitle, 0, 0, 1, 1)
         self.SongAlbum = QtWidgets.QLabel(self.gridLayoutWidget_3)
         font = QtGui.QFont()
         font.setFamily("Open Sans")
@@ -165,9 +165,9 @@ class Ui_MainWindow(object):
         self.SongAlbum.setFont(font)
         self.SongAlbum.setIndent(5)
         self.SongAlbum.setObjectName("SongAlbum")
-        self.gridLayout_2.addWidget(self.SongAlbum, 2, 0, 1, 1)
+        self.SongInfoAndControls.addWidget(self.SongAlbum, 2, 0, 1, 1)
         spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout_2.addItem(spacerItem2, 2, 1, 1, 1)
+        self.SongInfoAndControls.addItem(spacerItem2, 2, 1, 1, 1)
         self.SongArtist = QtWidgets.QLabel(self.gridLayoutWidget_3)
         font = QtGui.QFont()
         font.setFamily("Open Sans")
@@ -175,9 +175,9 @@ class Ui_MainWindow(object):
         self.SongArtist.setFont(font)
         self.SongArtist.setIndent(5)
         self.SongArtist.setObjectName("SongArtist")
-        self.gridLayout_2.addWidget(self.SongArtist, 1, 0, 1, 1)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.SongInfoAndControls.addWidget(self.SongArtist, 1, 0, 1, 1)
+        self.PlayerControls = QtWidgets.QHBoxLayout()
+        self.PlayerControls.setObjectName("PlayerControls")
         self.Prev = QtWidgets.QPushButton(self.gridLayoutWidget_3)
         self.Prev.setText("")
         icon1 = QtGui.QIcon()
@@ -187,7 +187,7 @@ class Ui_MainWindow(object):
         self.Prev.setDefault(False)
         self.Prev.setFlat(True)
         self.Prev.setObjectName("Prev")
-        self.horizontalLayout.addWidget(self.Prev)
+        self.PlayerControls.addWidget(self.Prev)
         self.Stop = QtWidgets.QPushButton(self.gridLayoutWidget_3)
         self.Stop.setText("")
         icon2 = QtGui.QIcon()
@@ -196,7 +196,7 @@ class Ui_MainWindow(object):
         self.Stop.setIconSize(QtCore.QSize(25, 25))
         self.Stop.setFlat(True)
         self.Stop.setObjectName("Stop")
-        self.horizontalLayout.addWidget(self.Stop)
+        self.PlayerControls.addWidget(self.Stop)
         self.PlayPause = QtWidgets.QPushButton(self.gridLayoutWidget_3)
         self.PlayPause.setText("")
         icon3 = QtGui.QIcon()
@@ -205,7 +205,7 @@ class Ui_MainWindow(object):
         self.PlayPause.setIconSize(QtCore.QSize(25, 25))
         self.PlayPause.setFlat(True)
         self.PlayPause.setObjectName("PlayPause")
-        self.horizontalLayout.addWidget(self.PlayPause)
+        self.PlayerControls.addWidget(self.PlayPause)
         self.Next = QtWidgets.QPushButton(self.gridLayoutWidget_3)
         self.Next.setText("")
         icon4 = QtGui.QIcon()
@@ -215,89 +215,27 @@ class Ui_MainWindow(object):
         self.Next.setDefault(False)
         self.Next.setFlat(True)
         self.Next.setObjectName("Next")
-        self.horizontalLayout.addWidget(self.Next)
-        self.gridLayout_2.addLayout(self.horizontalLayout, 0, 2, 2, 1)
+        self.PlayerControls.addWidget(self.Next)
+        self.SongInfoAndControls.addLayout(self.PlayerControls, 0, 2, 2, 1)
         self.Volume = QtWidgets.QHBoxLayout()
         self.Volume.setObjectName("Volume")
         self.VolumeDecr = QtWidgets.QPushButton(self.gridLayoutWidget_3)
-        self.VolumeDecr.setEnabled(False)
+        self.VolumeDecr.setEnabled(True)
         self.VolumeDecr.setText("")
         icon5 = QtGui.QIcon()
         icon5.addPixmap(QtGui.QPixmap("icons/ic_volume_down.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.VolumeDecr.setIcon(icon5)
-        self.VolumeDecr.setIconSize(QtCore.QSize(15, 15))
+        self.VolumeDecr.setIconSize(QtCore.QSize(25, 25))
         self.VolumeDecr.setFlat(True)
         self.VolumeDecr.setObjectName("VolumeDecr")
         self.Volume.addWidget(self.VolumeDecr)
-        self.VolumeSeekBar = QtWidgets.QSlider(self.gridLayoutWidget_3)
-        self.VolumeSeekBar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.VolumeSeekBar.setStyleSheet("QSlider::groove:horizontal {\n"
-"border: 1px solid #bbb;\n"
-"background: white;\n"
-"height: 10px;\n"
-"border-radius: 4px;\n"
-"}\n"
-"\n"
-"QSlider::sub-page:horizontal {\n"
-"background: qlineargradient(x1: 0, y1: 0,    x2: 0, y2: 1,\n"
-"    stop: 0 #66e, stop: 1 #bbf);\n"
-"background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1,\n"
-"    stop: 0 #bbf, stop: 1 #55f);\n"
-"border: 1px solid #777;\n"
-"height: 10px;\n"
-"border-radius: 4px;\n"
-"}\n"
-"\n"
-"QSlider::add-page:horizontal {\n"
-"background: #fff;\n"
-"border: 1px solid #777;\n"
-"height: 10px;\n"
-"border-radius: 4px;\n"
-"}\n"
-"\n"
-"QSlider::handle:horizontal {\n"
-"background: qlineargradient(x1:0, y1:0, x2:1, y2:1,\n"
-"    stop:0 #eee, stop:1 #ccc);\n"
-"border: 1px solid #777;\n"
-"width: 13px;\n"
-"margin-top: -2px;\n"
-"margin-bottom: -2px;\n"
-"border-radius: 4px;\n"
-"}\n"
-"\n"
-"QSlider::handle:horizontal:hover {\n"
-"background: qlineargradient(x1:0, y1:0, x2:1, y2:1,\n"
-"    stop:0 #fff, stop:1 #ddd);\n"
-"border: 1px solid #444;\n"
-"border-radius: 4px;\n"
-"}\n"
-"\n"
-"QSlider::sub-page:horizontal:disabled {\n"
-"background: #bbb;\n"
-"border-color: #999;\n"
-"}\n"
-"\n"
-"QSlider::add-page:horizontal:disabled {\n"
-"background: #eee;\n"
-"border-color: #999;\n"
-"}\n"
-"\n"
-"QSlider::handle:horizontal:disabled {\n"
-"background: #eee;\n"
-"border: 1px solid #aaa;\n"
-"border-radius: 4px;\n"
-"}")
-        self.VolumeSeekBar.setOrientation(QtCore.Qt.Horizontal)
-        self.VolumeSeekBar.setTickInterval(0)
-        self.VolumeSeekBar.setObjectName("VolumeSeekBar")
-        self.Volume.addWidget(self.VolumeSeekBar)
         self.VolumeIncr = QtWidgets.QPushButton(self.gridLayoutWidget_3)
-        self.VolumeIncr.setEnabled(False)
+        self.VolumeIncr.setEnabled(True)
         self.VolumeIncr.setText("")
         icon6 = QtGui.QIcon()
         icon6.addPixmap(QtGui.QPixmap("icons/ic_volume_up.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.VolumeIncr.setIcon(icon6)
-        self.VolumeIncr.setIconSize(QtCore.QSize(15, 15))
+        self.VolumeIncr.setIconSize(QtCore.QSize(25, 25))
         self.VolumeIncr.setFlat(True)
         self.VolumeIncr.setObjectName("VolumeIncr")
         self.Volume.addWidget(self.VolumeIncr)
@@ -310,8 +248,8 @@ class Ui_MainWindow(object):
         self.Mute.setFlat(True)
         self.Mute.setObjectName("Mute")
         self.Volume.addWidget(self.Mute)
-        self.gridLayout_2.addLayout(self.Volume, 2, 2, 1, 1)
-        self.gridLayout_3.addLayout(self.gridLayout_2, 0, 0, 1, 1)
+        self.SongInfoAndControls.addLayout(self.Volume, 2, 2, 1, 1)
+        self.Player.addLayout(self.SongInfoAndControls, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menuBar = QtWidgets.QMenuBar(MainWindow)
         self.menuBar.setGeometry(QtCore.QRect(0, 0, 705, 21))
@@ -353,11 +291,17 @@ class Ui_MainWindow(object):
         icon12.addPixmap(QtGui.QPixmap("icons/GitHub-Mark.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionSee_Source_on_Github.setIcon(icon12)
         self.actionSee_Source_on_Github.setObjectName("actionSee_Source_on_Github")
+        self.actionReport_Bug = QtWidgets.QAction(MainWindow)
+        icon13 = QtGui.QIcon()
+        icon13.addPixmap(QtGui.QPixmap("icons/ic_bug_report.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionReport_Bug.setIcon(icon13)
+        self.actionReport_Bug.setObjectName("actionReport_Bug")
         self.menuFile.addAction(self.actionOpen_File)
         self.menuEdit.addAction(self.actionFetch_Recommendation)
         self.menuEdit.addAction(self.actionEdit_MetaData)
         self.menuAbout.addAction(self.actionAbout_Project)
         self.menuAbout.addAction(self.actionSee_Source_on_Github)
+        self.menuAbout.addAction(self.actionReport_Bug)
         self.menuBar.addAction(self.menuFile.menuAction())
         self.menuBar.addAction(self.menuEdit.menuAction())
         self.menuBar.addAction(self.menuAbout.menuAction())
@@ -376,6 +320,8 @@ class Ui_MainWindow(object):
         self.Stop.setToolTip(_translate("MainWindow", "<html><head/><body><p>Stop</p></body></html>"))
         self.PlayPause.setToolTip(_translate("MainWindow", "<html><head/><body><p>Play/Pause</p></body></html>"))
         self.Next.setToolTip(_translate("MainWindow", "<html><head/><body><p>Next</p></body></html>"))
+        self.VolumeDecr.setToolTip(_translate("MainWindow", "<html><head/><body><p>Decrease Volume</p></body></html>"))
+        self.VolumeIncr.setToolTip(_translate("MainWindow", "<html><head/><body><p>Increase Volume</p></body></html>"))
         self.Mute.setToolTip(_translate("MainWindow", "<html><head/><body><p>Mute</p></body></html>"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
@@ -385,4 +331,5 @@ class Ui_MainWindow(object):
         self.actionFetch_Recommendation.setText(_translate("MainWindow", "Fetch Recommendation"))
         self.actionEdit_MetaData.setText(_translate("MainWindow", "Edit MetaData"))
         self.actionSee_Source_on_Github.setText(_translate("MainWindow", "See Source Code"))
+        self.actionReport_Bug.setText(_translate("MainWindow", "Report Bug"))
 
